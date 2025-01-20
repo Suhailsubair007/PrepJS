f7e870ecaceda622a275f26fc0b8d1948ccdd8f7
Suhailsubair007 <clashersuhail221@gmail.com>
2025-01-21 00:37:53 +0530

    new private counter code added

--- JavaScript/basics.js
+++ JavaScript/basics.js
@@ -68,32 +68,61 @@ const x = function (n) { // anonymous function
     return n * n;
 }
 
-// ===========================
-// First Class Functions
-// ===========================
-// Functions are first-class citizens, can act as variables, be passed as arguments, and return functions
+    // ===========================
+    // First Class Functions
+    // ===========================
+    // Functions are first-class citizens, can act as variables, be passed as arguments, and return functions
 
-// ===========================
-// IIFE (Immediately Invoked Function Expression)
-// ===========================
+    // ===========================
+    // IIFE (Immediately Invoked Function Expression)
+    // ===========================
 
-(function sample() {
-    console.log("iifeeeeeeeee");
-})();
+    // (function sample() {
+    //     console.log("iifeeeeeeeee");
+    // })();
 
 // Example:
-(function (x) {
-    return (function () {
-        console.log(x);
-    })();
-})(1);
+// (function (x) {
+//     return (function () {
+//         console.log(x);
+//     })();
+// })(1);
 
 // ===========================
 // Closure
 // ===========================
 
-for (let i = 0; i < 3; i++) { // changed var to let to create block scope
-    setTimeout(() => {
-        console.log(i);
-    }, 1000);
+// for (let i = 0; i < 3; i++) { // changed var to let to create block scope
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 1000);
+// }
+
+// ===========================
+// Closure Example with Private Counter
+// ===========================
+
+// Private counter using closure
+function counter() {
+    let counter = 0; // private variable
+    function add(inc) {
+        counter += inc; // increment counter
+    }
+    function dec(dec) {
+        counter -= dec; // decrement counter
+    }
+    function retrieve() {
+        return "counter: " + counter; // retrieve current counter value
+    }
+    return {
+        add,
+        dec,
+        retrieve
+    };
 }
+
+const c = counter();
+c.add(5); // add 5 to counter
+c.add(10); // add 10 to counter
+c.dec(2); // subtract 2 from counter
+console.log(c.retrieve()); // output the current counter value
